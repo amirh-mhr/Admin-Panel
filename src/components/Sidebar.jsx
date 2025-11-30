8e5538f79a67cfc1389a23e18715b7f0bf5ce591
Amir Mehrani <amirhosseinmehrani75@gmail.com>
2025-11-30 21:05:05 +0330

    sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,23 @@
 import React from "react"
+import { useStateContext } from "../contexts/contextProvider"
 
 const Sidebar = () => {
+    const { activeMenu, setActiveMenu, screenSize, currentColor } = useStateContext();
+
+    const handleclicksidebar = () => {
+        if(activeMenu && screenSize <= 1024) {
+            setActiveMenu(false);
+        }
+        
+    }
     return (
-        <div>Sidebar</div>
+        <div className="mr-3 h-screen md:overflow-hidden overflow-auto
+        md:hover:overflow-auto pb-10">
+            
+        </div>
     )
 }
 
 
 export default Sidebar
-
 
