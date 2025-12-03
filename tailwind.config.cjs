d754bd517f39a963972078e3e6c30a9c51014839
Amir Mehrani <amirhosseinmehrani75@gmail.com>
2025-12-03 23:59:45 +0330

    DEbug

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -12,10 +12,10 @@ module.exports = {
         14: "14px",
       },
       backgroundColor: {
-        "main-bg": "#FAFBFB",
+        "main-bg": "#ffffffff",
         "main-dark-bg": "#20232A",
         "secondary-dark-bg": "#33373E",
-        "light-gray": "#F7F7F7",
+        "light-gray": "#ffffffff",
         "half-transparent": "rgba(0, 0, 0, 0.5)",
       },
       borderWidth: {
