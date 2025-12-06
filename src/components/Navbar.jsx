154df78cadebf841925ec3221e7411b1718f6dc0
Amir Mehrani <amirhosseinmehrani75@gmail.com>
2025-12-06 19:24:56 +0330

    NavButton

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect } from "react";
 import { useStateContext } from "../contexts/contextProvider";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+import { AiOutlineMenu } from "react-icons/ai";
 
-const navButton = ({ title, cunstomFunc, icon, color, dotColor }) => (
+const NavButton = ({ title, cunstomFunc, icon, color, dotColor }) => (
   <TooltipComponent content={title} position="BottomLeft">
     <button
       className="relative text-xl rounded-full p-3 hover:bg-gray-100"
@@ -10,7 +11,11 @@ const navButton = ({ title, cunstomFunc, icon, color, dotColor }) => (
       style={{ color }}
       onClick={cunstomFunc}
     >
-    <span className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2" style={{background: dotColor}} />        
+      <span
+        className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2"
+        style={{ background: dotColor }}
+      />
+      {icon}
     </button>
   </TooltipComponent>
 );
@@ -40,7 +45,14 @@ function Navbar() {
       className="flex dark:shadow-gray-800 shadow-sm 
   justify-between p-2 shadow-gray-200 relative"
     >
-      Navbar
+      {screenSize <= 1024 && (
+        <NavButton
+          title="منو"
+          cunstomFunc={() => setActiveMenu((prev) => !prev)}
+          color={currentColor}
+          icon={<AiOutlineMenu />}
+        />
+      )}
     </div>
   );
 }
