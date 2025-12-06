4b22a0402aedaece09ebbc429dbb30fd2b23158d
Amir Mehrani <amirhosseinmehrani75@gmail.com>
2025-12-06 18:39:01 +0330

    Navbuttons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,19 @@
 import React, { useEffect } from "react";
 import { useStateContext } from "../contexts/contextProvider";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+
+const navButton = ({ title, cunstomFunc, icon, color, dotColor }) => (
+  <TooltipComponent content={title} position="BottomLeft">
+    <button
+      className="relative text-xl rounded-full p-3 hover:bg-gray-100"
+      type="button"
+      style={{ color }}
+      onClick={cunstomFunc}
+    >
+    <span className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2" style={{background: dotColor}} />        
+    </button>
+  </TooltipComponent>
+);
 
 function Navbar() {
   const { setActiveMenu, screenSize, setScreenSize, currentColor, isClicked } =
@@ -19,9 +33,16 @@ function Navbar() {
     if (screenSize <= 900) {
       setActiveMenu(false);
     } else setActiveMenu(true);
-  },[screenSize]); 
+  }, [screenSize]);
 
-  return <div>Navbar</div>;
+  return (
+    <div
+      className="flex dark:shadow-gray-800 shadow-sm 
+  justify-between p-2 shadow-gray-200 relative"
+    >
+      Navbar
+    </div>
+  );
 }
 
 export default Navbar;
